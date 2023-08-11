783f08bb7432d46ad4e326bcef9b54d64c575296
siphesihlemhlongo <sphesihlemhlongo07@gmail.com>
2023-08-11 13:38:51 -0700

    Python - import & modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+"""Print the addition of all arguments"""
+import sys
+
+total = 0
+for a in range(len(sys.argv) - 1):
+total += int(sys.argv[a + 1])
+print("{}".format(total))
